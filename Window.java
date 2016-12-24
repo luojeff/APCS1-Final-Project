875f8b5fb81f34c00724a97c2ab58533186584eb
Jeffrey Luo <luojeffrey@yahoo.com>
2016-12-24 16:22:07 -0500

    Changed MenuBar feature and cleaned some code

--- Window.java
+++ Window.java
@@ -3,29 +3,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Window extends JFrame {
+    private JMenuBar menuBar;
     private JMenu menu;
     private Editor textField;
     private Container pane;
 
     public Window(){
-	this.setTitle("Text Editor");
-	this.setSize(1200,800);
+        this.setTitle("Text Editor");
+        this.setSize(1200,800);
 	this.setLocation(150,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
 
-	GridLayout g = new GridLayout(2,2);
-	// For sake of simplicity, can be changed later
-        pane.setLayout(g);
+	JPanel top = new JPanel(new BorderLayout());
+	JPanel bottom = new JPanel(new BorderLayout());
+	JPanel both = new JPanel();
+	both.setLayout(new BoxLayout(both, BoxLayout.PAGE_AXIS));
+	both.add(top);
+	both.add(bottom);
+	pane.add(both);
 
-	menu = new JMenu("Menubar");
-	JMenuItem mi = new JMenuItem("File");
-	menu.add(mi);
+	// Creates a menubar on the top which can contain the common
+	// functions of most editors (File, Edit, Options, Help, etc...)
+	menuBar = new JMenuBar();
+	menu = new JMenu("Edit");
+	menuBar.add(menu);
+	
+	JMenuItem menui1 = new JMenuItem("Copy");
+	JMenuItem menui2 = new JMenuItem("Paste");
+	menu.add(menui1);
+	menu.add(menui2);
 	
 	textField = new Editor();
 
-	pane.add(menu);
-	pane.add(textField);
+	top.add(menuBar);
+	bottom.add(textField);
     }
 }
