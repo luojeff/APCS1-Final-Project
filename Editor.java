04a8154d38f4a66a4994225b342bfbd4e6c9b772
Jeffrey Luo <luojeffrey@yahoo.com>
2016-12-24 21:30:00 -0500

    Fixed alignment issue and added JEditorPane

--- Editor.java
+++ Editor.java
@@ -6,10 +6,10 @@ import java.awt.event.*;
  * Main component for displaying the area for editing text.
  * Receives actions and allows for interaction.
  */
-public class Editor extends JTextArea implements ActionListener {
-
+public class Editor extends JEditorPane implements ActionListener {
     public Editor(){
-	super(10,50);
+	setFont(new Font("Arial", Font.PLAIN, 18));
+	//setLineWrap(true);
     }
     
     public void actionPerformed(ActionEvent e){
--- Window.java
+++ Window.java
@@ -4,19 +4,19 @@ import java.awt.event.*;
 
 public class Window extends JFrame {
     private JMenuBar menuBar;
-    private JMenu menu;
-    private Editor textField;
+    private JMenu menu1, menu2, menu3;
+    private Editor editField;
     private Container pane;
 
     public Window(){
         this.setTitle("Text Editor");
         this.setSize(1200,800);
-	this.setLocation(150,100);
+	this.setLocation(300,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
 
-	JPanel top = new JPanel(new BorderLayout());
+	JPanel top = new JPanel(new FlowLayout(FlowLayout.LEFT));
 	JPanel bottom = new JPanel(new BorderLayout());
 	JPanel both = new JPanel();
 	both.setLayout(new BoxLayout(both, BoxLayout.PAGE_AXIS));
@@ -27,17 +27,28 @@ public class Window extends JFrame {
 	// Creates a menubar on the top which can contain the common
 	// functions of most editors (File, Edit, Options, Help, etc...)
 	menuBar = new JMenuBar();
-	menu = new JMenu("Edit");
-	menuBar.add(menu);
+	menu1 = new JMenu("File");
+	menu2 = new JMenu("Edit");
+	menu3 = new JMenu("Help");
+	menuBar.add(menu1);
+	menuBar.add(menu2);
+	menuBar.add(menu3);
+
+	JMenuItem m1i1 = new JMenuItem("Save");
+	JMenuItem m1i2 = new JMenuItem("Exit");
+	JMenuItem m2i1 = new JMenuItem("Copy");
+	JMenuItem m2i2 = new JMenuItem("Paste");
+	JMenuItem m3i1 = new JMenuItem("About");
 	
-	JMenuItem menui1 = new JMenuItem("Copy");
-	JMenuItem menui2 = new JMenuItem("Paste");
-	menu.add(menui1);
-	menu.add(menui2);
+	menu1.add(m1i1);
+	menu1.add(m1i2);
+	menu2.add(m2i1);
+	menu2.add(m2i2);
+	menu3.add(m3i1);
 	
-	textField = new Editor();
+	editField = new Editor();
 
 	top.add(menuBar);
-	bottom.add(textField);
+	bottom.add(editField);
     }
 }
