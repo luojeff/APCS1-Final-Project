d32445c8fbf7dcbfa6dca2246f827fda5930428f
Max Z <max.zlotskiy@gmail.com>
2016-12-31 13:19:18 -0500

    Fixed indentation to 4 spaces

--- EditorArea.java
+++ EditorArea.java
@@ -11,42 +11,42 @@ public class EditorArea extends JPanel {
     JEditorPane ePane;
     
     public EditorArea(){
-	super(new BorderLayout());
-	
-	ePane = new JEditorPane();
-	ePane.setFont(new Font("Consolas", Font.PLAIN, 18));
+        super(new BorderLayout());
+        
+        ePane = new JEditorPane();
+        ePane.setFont(new Font("Consolas", Font.PLAIN, 18));
 
-	// Demo: ePane.getInputMap().put(KeyStroke.getKeyStroke("A"),"keyA");
-	// ePane.getActionMap().put("keyA", new KeyAction("A"));
-	ePane.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_C,KeyEvent.CTRL_MASK),"keyCopy");
-	ePane.getActionMap().put("keyCopy", new KeyAction("copy"));
-	
-	this.add(ePane);
+        // Demo: ePane.getInputMap().put(KeyStroke.getKeyStroke("A"),"keyA");
+        // ePane.getActionMap().put("keyA", new KeyAction("A"));
+        ePane.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_C,KeyEvent.CTRL_MASK),"keyCopy");
+        ePane.getActionMap().put("keyCopy", new KeyAction("copy"));
+        
+        this.add(ePane);
     }
 
     public String getText(){
-	return ePane.getText();
+        return ePane.getText();
     }
 
     public void setText(String contents){
-	ePane.setText(contents);
+        ePane.setText(contents);
     }
-				 
+                 
     /**
      *Action command for when a certain
      *key (or binding) is pressed. 
      */
     public static class KeyAction extends AbstractAction {
-	private String sequence;
-	
-	public KeyAction(String sequence){
-	    this.sequence = sequence;
-	};
+        private String sequence;
+        
+        public KeyAction(String sequence){
+            this.sequence = sequence;
+        };
 
-	public void actionPerformed(ActionEvent e){ 
-	    if(sequence.equals("copy")){
-		System.out.println("Highlighted text copied!");
-	    }
-	}
+        public void actionPerformed(ActionEvent e){ 
+            if(sequence.equals("copy")){
+                System.out.println("Highlighted text copied!");
+            }
+        }
     }
 }
