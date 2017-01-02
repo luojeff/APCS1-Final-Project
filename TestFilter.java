1359fa6ead5cddb0931c65f9f3784a7bf23428a3
Max Z <max.zlotskiy@gmail.com>
2017-01-02 15:31:44 -0500

    Will now change every third character to bold

--- TestFilter.java
+++ TestFilter.java
@@ -4,4 +4,15 @@ public class TestFilter extends DocumentFilter {
     public TestFilter() {
         super();
     }
+
+    public void insertString(FilterBypass fb, int offset, String str, AttributeSet attr) throws BadLocationException {
+        System.out.println("invoked");
+        if(offset % 3 == 0) {
+            SimpleAttributeSet bold = new SimpleAttributeSet(attr);
+            StyleConstants.setBold(bold, true);
+            super.insertString(fb, offset, str, bold);
+        } else {
+            super.insertString(fb, offset, str, attr);
+        }
+    }
 }
