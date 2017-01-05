5d967c9146b8191893654c6a06267f19e26f32b8
Jeffrey Luo <luojeffrey@yahoo.com>
2017-01-05 12:03:35 -0500

    Fixed AttributeSet

--- TestFilter.java
+++ TestFilter.java
@@ -1,4 +1,5 @@
 import javax.swing.text.*;
+import java.awt.*;
 
 public class TestFilter extends DocumentFilter {
     public TestFilter() {
@@ -7,12 +8,28 @@ public class TestFilter extends DocumentFilter {
 
     public void replace(FilterBypass fb, int offset, int length, String str, AttributeSet attr) throws BadLocationException {
         System.out.println("invoked");
-	SimpleAttributeSet bold = new SimpleAttributeSet(attr);
-        if(offset > 10 && offset < 15) {
-            StyleConstants.setBold(bold, true);
-            super.replace(fb, offset, length, str, bold);
+	SimpleAttributeSet attrs = new SimpleAttributeSet(attr);
+
+	String keyWord = "class";
+	
+        if(offset % 2 == 0) {
+            StyleConstants.setBold(attrs, true);
+	    StyleConstants.setForeground(attrs, Color.red);
+            super.replace(fb, offset, length, str, attrs);
         } else {
-            super.replace(fb, offset, length, str, attr);
+	    StyleConstants.setBold(attrs, false);
+	    StyleConstants.setForeground(attrs, Color.black);
+            super.replace(fb, offset, length, str, attrs);
+	    
         }
+
+	/*
+
+	int ind = EditorArea.getEditor().indexOf(keyWord);
+	if(ind != 0){
+	    StyleConstants.setBold(bold, true);
+	    super.replace(fb, ind, keyWord.length(), keyWord, bold);
+	}
+	*/
     }
 }
