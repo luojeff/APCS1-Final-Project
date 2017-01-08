a1c6792e87b7ff6bde8f74b888d431ad64497602
Max Z <max.zlotskiy@gmail.com>
2017-01-07 19:24:05 -0500

    Figured out how to change Element names

--- TestFilter.java
+++ TestFilter.java
@@ -8,10 +8,10 @@ public class TestFilter extends DocumentFilter {
 
     public void replace(FilterBypass fb, int offset, int length, String str, AttributeSet attr) throws BadLocationException {
         System.out.println("invoked");
-	SimpleAttributeSet attrs = new SimpleAttributeSet(attr);
-	attrs.removeAttribute(AttributeSet.NameAttribute);
-	attrs.addAttribute(AttributeSet.NameAttribute, "customName");
-	super.replace(fb, offset, length, str, attrs);
+	    SimpleAttributeSet attrs = new SimpleAttributeSet(attr);
+	    attrs.removeAttribute(AbstractDocument.ElementNameAttribute);
+	    attrs.addAttribute(AbstractDocument.ElementNameAttribute, "customName");
+	    super.replace(fb, offset, length, str, attrs);
         /*if(offset % 2 == 0) {
             StyleConstants.setBold(attrs, true);
 	    StyleConstants.setForeground(attrs, Color.red);
