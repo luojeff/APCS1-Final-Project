a25b8ed9f4b55b3d795185fcdfc10bf48f9b1681
Max Z <max.zlotskiy@gmail.com>
2017-01-12 12:33:11 -0500

    Adding constructors for printing; turns out empty one is used; insert is never called

--- SyntaxHighlighterDoc.java
+++ SyntaxHighlighterDoc.java
@@ -1,5 +1,19 @@
 import javax.swing.text.*;
 
 public class SyntaxHighlighterDoc extends DefaultStyledDocument {
+    protected void insert(int offset, DefaultStyledDocument.ElementSpec[] data){
+        System.out.println("Insert (" + offset + ", " + data.length);
+    }
 
+    public SyntaxHighlighterDoc() {
+	System.out.println("Empty constructor");
+    }
+
+    public SyntaxHighlighterDoc(AbstractDocument.Content c, StyleContext styles) {
+        System.out.println("Content and styles");
+    }
+
+    public SyntaxHighlighterDoc(StyleContext styles) {
+        System.out.println("Just styles");
+    }
 }
