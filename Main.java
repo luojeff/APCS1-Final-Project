ef269fccf58ded2310ccbb61e5b2c4360cd38877
Jeffrey Luo <luojeffrey@yahoo.com>
2017-01-13 12:19:40 -0500

    Added lookAndFeel options

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
-import javax.swing.JEditorPane;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
         setup();
-	    Window w = new Window();
-	    w.setVisible(true);
+	Window w = new Window();
+	w.setVisible(true);
+	
+	try {
+	    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+	} catch (UnsupportedLookAndFeelException e) {
+	} catch (ClassNotFoundException e) {
+	} catch (InstantiationException e) {
+	} catch (IllegalAccessException e) {
+	}
     }
 
     /**
-    * Method to load plugins, register EditorKits, etc
-    * before window creation.
-    */
+     * Method to load plugins, register EditorKits, etc
+     * before window creation.
+     */
     public static void setup() {
         JEditorPane.registerEditorKitForContentType("text/html_code", "HtmlCodeKit");
     }
