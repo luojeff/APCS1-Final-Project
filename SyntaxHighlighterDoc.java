358a4505d0cc3eb77f364fc64799c24efff7cd31
Max Z <max.zlotskiy@gmail.com>
2017-01-12 22:15:42 -0500

    Added more methods to sniff, nothing has budged

--- SyntaxHighlighterDoc.java
+++ SyntaxHighlighterDoc.java
@@ -1,19 +1,27 @@
 import javax.swing.text.*;
 
 public class SyntaxHighlighterDoc extends DefaultStyledDocument {
-    protected void insert(int offset, DefaultStyledDocument.ElementSpec[] data){
+    protected void insert(int offset, DefaultStyledDocument.ElementSpec[] data) throws BadLocationException {
+        super.insert(offset, data);
         System.out.println("Insert (" + offset + ", " + data.length);
     }
 
+    public void insertString(int offset, String str, AttributeSet a) throws BadLocationException {
+        super.insertString(offset, str, a);
+        System.out.println("inserString("+offset+","+str+")");
+    }
+
     public SyntaxHighlighterDoc() {
-	System.out.println("Empty constructor");
+	    System.out.println("Empty constructor");
     }
 
     public SyntaxHighlighterDoc(AbstractDocument.Content c, StyleContext styles) {
+        super(c, styles);
         System.out.println("Content and styles");
     }
 
     public SyntaxHighlighterDoc(StyleContext styles) {
+        super(styles);
         System.out.println("Just styles");
     }
 }
