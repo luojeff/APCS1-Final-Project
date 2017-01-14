8d67b08bbf9af9e488206eacd7d22f96bf0d7ca6
Max Z <max.zlotskiy@gmail.com>
2017-01-14 12:51:43 -0500

    Turns all inserted characters red; all other strategies failed except for insertUpdate

--- SyntaxHighlighterDoc.java
+++ SyntaxHighlighterDoc.java
@@ -1,4 +1,5 @@
 import javax.swing.text.*;
+import java.awt.Color;
 
 public class SyntaxHighlighterDoc extends DefaultStyledDocument {
     private Theme theme;
@@ -11,7 +12,34 @@ public class SyntaxHighlighterDoc extends DefaultStyledDocument {
     //this one gets used by the system
     protected void insertUpdate(AbstractDocument.DefaultDocumentEvent evt, AttributeSet set) {
         System.out.println("insertUpdate");
-        super.insertUpdate(evt, set);
+        int offset = ((evt.getOffset() == 0)? 0 : evt.getOffset() - 1), length = evt.getLength();
+        System.out.println("off: "+offset+", len: "+length+", doc:"+this.getLength());
+        SimpleAttributeSet attrs = new SimpleAttributeSet(set);
+        attrs.addAttribute(AbstractDocument.ElementNameAttribute, "testing");
+        StyleConstants.setForeground(attrs, Color.red);
+        super.insertUpdate(evt, attrs);
+        // try {
+        //     this.insert(offset, new DefaultStyledDocument.ElementSpec[] {
+        //         elemSpec(attrs, "", "", length)
+        //     });
+        // } catch(BadLocationException e) {
+        //     System.out.println("Error");
+        //     e.printStackTrace();
+        // }
+    }
+
+    private DefaultStyledDocument.ElementSpec elemSpec(AttributeSet set, String type, String dir, int len) {
+        DefaultStyledDocument.ElementSpec result;
+        short t = DefaultStyledDocument.ElementSpec.ContentType;
+        short d = DefaultStyledDocument.ElementSpec.OriginateDirection;
+        if(type.equals("start")) {t = DefaultStyledDocument.ElementSpec.StartTagType;}
+        else if(type.equals("end")) {t = DefaultStyledDocument.ElementSpec.EndTagType;}
+        if(dir.equals("fracture")) {t = DefaultStyledDocument.ElementSpec.JoinFractureDirection;}
+        else if(dir.equals("next")) {t = DefaultStyledDocument.ElementSpec.JoinNextDirection;}
+        else if(dir.equals("prev")) {t = DefaultStyledDocument.ElementSpec.JoinPreviousDirection;}
+        result = new DefaultStyledDocument.ElementSpec(set, t, len);
+        result.setDirection(d);
+        return result;
     }
 
     public void insertString(int offset, String str, AttributeSet a) throws BadLocationException {
