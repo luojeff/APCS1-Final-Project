fc069bbb12d5b2bb171c30aa316cd49c4c715746
Max Z <max.zlotskiy@gmail.com>
2017-01-16 12:19:59 -0500

    Created class to move parsing code out of the Document class

---
+++ HtmlSyntax.java
@@ -0,0 +1,29 @@
+import javax.swing.text.*;
+import javax.swing.text.AbstractDocument.*;
+
+public class HtmlSyntax {
+
+    public static class Token {
+        public int start;
+        public int end;
+        public String name;
+
+        public Token(String n, int s, int e) {
+            name = n;
+            start = s;
+            end = e;
+        }
+
+        public String toString() {
+            return name + "[" + start + ":" + end + "]";
+        }
+
+        public boolean equals(Token other) {
+            return (other.name.equals(this.name) && other.start == this.start && other.end == this.end);
+        }
+
+        public Token copy() {
+            return new Token(name, start, end);
+        }
+    }
+}
